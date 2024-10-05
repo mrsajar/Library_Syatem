74e65301da165cf20ddbb71a46d334bf76b1e6be
mesajar <mssaifi386@gmail.com>
2024-10-05 20:17:22 +0530

    fix swagger error

---
+++ src/main/java/Library/System/Library/System/Configuration/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package Library.System.Library.System.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library System API")
+                        .version("1.0")
+                        .description("API documentation for the Library System"));
+    }
+}
